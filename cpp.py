240fe01723d02317b1f18177dc2a1985c0ab6313
Manish  Vohra <manish.vohra@gmail.com>
2023-08-05 11:40:02 +0530

    Add files via upload

---
+++ cpp.py
@@ -0,0 +1,39 @@
+import os
+import shutil
+
+
+def list_items_in_directory(directory_path):
+    try:
+       
+        items = os.listdir(directory_path)
+        for dirs in items:
+            #print(dirs)
+            if "." in dirs or "destination" in dirs:
+                continue
+            else:
+                os.chdir(dirs)
+                files = os.listdir(os.getcwd())
+                print(files)
+                print(os.getcwd())
+                for item in files:
+                    if ".cpp" in item:
+                        print(os.getcwd()+"\\"+item)
+                        source_file_path = os.getcwd()+"\\"+item
+                        destination_file_path = directory_path+"\\destination"
+                        try:
+                            shutil.copy(source_file_path, destination_file_path)
+                            print("File moved successfully!")
+                        except shutil.Error as e:
+                            print(f"Error while moving the file: {e}")
+                        except FileNotFoundError:
+                            print("Source file not found.")
+            os.chdir(directory_path)
+
+    except OSError as e:
+        print(f"An error occurred: {e}")
+
+ 
+ 
+
+path_to_directory = os.getcwd()
+list_items_in_directory(path_to_directory)
